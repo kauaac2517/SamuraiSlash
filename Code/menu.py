5cb675e8cd43f5a95a17ca500b7da4af6f1271c2
NOC - Kauã <kaua.almeida.cardoso@gmail.com>
2025-03-25 20:52:37 -0300

    Reajustando imagem e som na janela

--- Code/menu.py
+++ Code/menu.py
@@ -1,11 +1,13 @@
 import pygame.image
+#from pygame import Surface, Rect
 
 
 class Menu:
     def __init__(self, window):
         self.window = window
-        self.surf = pygame.image.load('./assets/BG.png')
-        self.rect = self.surf.get_rect(left=0, right=0)
+        self.surf = pygame.image.load("./assets/background.png")
+        self.surf = pygame.transform.scale(self.surf, (960, 540))
+        self.rect = self.surf.get_rect(left=0, top=0)
 
     def run(self, ):
         pygame.mixer_music.load('./assets/bgMusic.mpeg')
@@ -14,6 +16,7 @@ class Menu:
         while True:
             self.window.blit(source=self.surf, dest=self.rect)
             pygame.display.flip()
+
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                    pygame.quit()
