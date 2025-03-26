96c4abb14a4f41295a2a0127e5972b6ea782deb1
NOC - Kauã <kaua.almeida.cardoso@gmail.com>
2025-03-25 22:12:51 -0300

    Reajustando imagem e som na janela

--- Code/menu.py
+++ Code/menu.py
@@ -1,6 +1,6 @@
 import pygame.image
-#from pygame import Surface, Rect
-
+from pygame import Surface, Rect
+from pygame.font import Font
 
 class Menu:
     def __init__(self, window):
@@ -15,9 +15,21 @@ class Menu:
 
         while True:
             self.window.blit(source=self.surf, dest=self.rect)
+            self.menu_text(100, "Samurai", (140, 38, 114), ((960 / 2), 70))
+            self.menu_text(100, "Slash", (140, 38, 114), ((960 / 2), 150))
+
+
             pygame.display.flip()
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                    pygame.quit()
                    quit()
+
+    def menu_text(self, text_size: int, text: str, text_color: tuple, text_center_pos: tuple):
+        text_font: Font = pygame.font.SysFont(name = "Open Sans", size = text_size)
+        text_surf: Surface = text_font.render(text, True, text_color).convert_alpha()
+        text_rect: Rect = text_surf.get_rect(center = text_center_pos)
+        self.window.blit(source = text_surf, dest = text_rect)
+
+        pygame.font.get_fonts()
