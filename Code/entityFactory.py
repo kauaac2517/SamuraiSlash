f1314f97ab8bf4c7206c3372ecb87d0ab9428e0f
NOC - Kauã <kaua.almeida.cardoso@gmail.com>
2025-03-27 21:03:17 -0300

    Adicionado key events e organizando código para adicionar a gameplay

--- Code/entityFactory.py
+++ Code/entityFactory.py
@@ -2,11 +2,12 @@ from Code.background import Background
 
 
 class EntityFactory:
+
     @staticmethod
     def get_entity(entity_name: str, position = (0,0)):
         match entity_name:
             case "bgInGame":
                 list_bg=[]
                 for i in range(1):
-                    list_bg.append(Background("bgInGame", (0, 0)))
+                    list_bg.append(Background(f"bgInGame{i}", (0, 0)))
                 return list_bg
